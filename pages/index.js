03453049a27368cf7be7d5083ebdabbd6e0af384
maron5274 <1521517@ed.tus.ac.jp>
2022-07-13 02:50:43 +0900

    commit

--- pages/index.js
+++ pages/index.js
@@ -59,9 +59,9 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <span className={styles.logo}>
+          {/* <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          </span> */}
         </a>
       </footer>
     </div>
